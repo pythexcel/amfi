a792aedb787ec4c48dc27bdab3fdf4721772e05f
etech <email@gmail.com>
2019-07-14 08:36:18 +0530

    fix

---
+++ stats/jobs/returns/1yr.py
@@ -0,0 +1,7 @@
+
+
+# this will caculate 1yr return of all funds
+# problem is nav is daily calculated and 1yr return will change daily
+# need to find an effecient way to do this fast accross all schemes
+def one_year_abs_return():
+    pass
