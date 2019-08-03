2ed2ec449ad3599479a37de3ce0ee0eaf3f28e1b
etech <email@gmail.com>
2019-08-03 11:27:10 +0530

    fix

--- amc/jobs/portfolio_process.py
+++ amc/jobs/portfolio_process.py
@@ -410,7 +410,7 @@ def process_portfolio(filename, amc, date, amc_process):
                                 print(e)
                                 print(scheme_data)
                                 print(isin)
-                                traceback.print_exc(e)
+                                # traceback.print_exc(e)
                                 pass
                             
 
