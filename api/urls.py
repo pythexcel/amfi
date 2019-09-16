710684c3946933c85145f2bd9e021e5dfc8967cc
etech <pythexcel@excellencetechnologies.in>
2019-09-16 19:53:01 +0530

    nav and schemes list api

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include, re_path as url
 from api.views.returns import rolling_return, abs_return
 from api.views.ping import ping
 from api.views.funds import ListAmc, get_amcs, get_schemes, get_fund_categories, get_fund_subcategories, get_funds
-from api.views.dashboard import nav_check, nav_run_script, get_process_logs, index_check, index_run_script
+from api.views.dashboard import (nav_check, nav_run_script, get_process_logs, index_check, index_run_script,nav_last_update,schem_list,
+schem_update_list,nav_ten)
 from rest_framework.routers import DefaultRouter
 
 
@@ -14,6 +15,10 @@ urlpatterns = [
     url(r'^return/abs/(?P<amfi>\d+)/$', abs_return, name='abs_return'),
     url(r'^ping', ping),
     url(r'^amc',get_amcs),
+    url(r'^unupdated_nav',nav_ten),
+    url(r'^updated_scheme',schem_update_list),
+    url(r'^nav_last/(?P<scheme_id>\d+)',nav_last_update),
+    url(r'^scheme',schem_list),
     url(r'^funds/amc', ListAmc.as_view()),
     url(r'^funds/scheme/(?P<amc_id>\d+)/$', get_schemes),
     url(r'^funds/category', get_fund_categories),
--- api/views/dashboard.py
+++ api/views/dashboard.py
@@ -7,11 +7,20 @@ from rest_framework import generics
 from todo.jobs.health_check import nav_check as nav_check_data, index_check as index_check_data
 from todo.jobs import schedule_daily_nav_download, process_nse_historial, process_bse_historial
 
-import datetime
+from datetime import datetime,timedelta
 
 import sys
 
 from todo.logs import get_logs
+from todo.models import AMC,Nav,Scheme,NavSerializer
+from todo.serializers import AMCSerializer, SchemeSerializer
+
+
+@api_view()
+def get_amcs(request):
+    ret = AMC.objects.all()
+    ser = AMCSerializer(ret, many=True)
+    return Response(ser.data)
 
 
 @api_view()
@@ -41,3 +50,59 @@ def index_run_script(request):
     process_nse_historial.modify(next_run_time=datetime.datetime.now())
     process_bse_historial.modify(next_run_time=datetime.datetime.now())
     return Response([])
+
+
+@api_view()
+def schem_list(request):
+    schem = Scheme.objects.all()
+    serial = SchemeSerializer(schem,many=True)
+    return Response(serial.data)     
+
+
+@api_view(['POST'])
+def nav_last_update(request,scheme_id=None):
+    nav = Nav.objects.filter(scheme=scheme_id)
+    serializer = NavSerializer(nav,many=True) 
+    return Response(serializer.data)
+
+@api_view()
+def schem_update_list(request):
+    date = datetime.now() - timedelta(1)
+    nav = Nav.objects.filter(date__gte=date)
+    serializer = NavSerializer(nav,many=True) 
+    unique_nav = []
+    for data in serializer.data:
+        if data['scheme'] not in unique_nav:
+            unique_nav.append(data['scheme'])
+        else:
+            pass
+    updated_schemes = []    
+    for element in unique_nav:
+        schem = Scheme.objects.get(id=element)
+        ser = SchemeSerializer(schem,many=False)
+        updated_schemes.append(ser.data['fund_name'])
+    ret = ",".join(updated_schemes) 
+    return Response(ret + " These are not updated")   
+    return Response(serializer.data)  
+
+@api_view()
+def nav_ten(request):
+    date = datetime.now() - timedelta(10)
+    nav = Nav.objects.filter(date__lte=date)
+    serializer = NavSerializer(nav,many=True)
+    unique_nav = []
+    for data in serializer.data:
+        if data['scheme'] not in unique_nav:
+            unique_nav.append(data['scheme'])
+        else:
+            pass
+    un_updated_schemes = []    
+    for element in unique_nav:
+        schem = Scheme.objects.get(id=element)
+        ser = SchemeSerializer(schem,many=False)
+        un_updated_schemes.append(ser.data['fund_name'])
+    ret = ",".join(un_updated_schemes) 
+    return Response(ret + " These are not updated from last ten days")   
+
+
+    
